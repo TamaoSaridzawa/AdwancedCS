072971a17c4d131d4a926ea804f073531c5fa27d
TamaoSaridzawa <85618999+TamaoSaridzawa@users.noreply.github.com>
2022-06-09 20:56:52 +0300

    Добавьте файлы проекта.

---
+++ AdwancedCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdwancedCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
