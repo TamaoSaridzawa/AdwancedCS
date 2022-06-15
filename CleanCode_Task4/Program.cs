98b7ab6e765cbd95d43315025c1108133cf97bd1
TamaoSaridzawa <85618999+TamaoSaridzawa@users.noreply.github.com>
2022-06-15 12:21:53 +0300

    008_CleanCode_Task4

---
+++ CleanCode_Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CleanCode_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armySize = 10;
+            int coinsNumber = 10;
+            string name = "Vladislav";
+        }
+    }
+}
