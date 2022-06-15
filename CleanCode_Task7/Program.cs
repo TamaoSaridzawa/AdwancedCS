ac5669bcd8f4a6f18d0ba3a2f21c15067009c0f2
TamaoSaridzawa <85618999+TamaoSaridzawa@users.noreply.github.com>
2022-06-15 14:12:07 +0300

    011_CleanCode_Task7

---
+++ CleanCode_Task7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CleanCode_Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void CreateNewObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void SetRandomValue()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int GetCalculatedSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
+}
