2e79a1829c4f8d05fd8cc52fddce3f05fe402737
TamaoSaridzawa <85618999+TamaoSaridzawa@users.noreply.github.com>
2022-06-15 13:14:13 +0300

    010_CleanCode_Task6

---
+++ CleanCode_Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CleanCode_Task6
+{
+    class Player { }
+
+    class Gun { }
+
+    class Stalker { }
+
+    class Unit { }
+
+    class Army
+    {
+        public IReadOnlyCollection<Unit> UnitsToGet { get; private set; }
+    }
+}
